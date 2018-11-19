f37014840201f13271a03ccbd38b8a5e871c3c1b
Antony Chan <antony.chan@oocl.com>
2018-11-19 16:40:22 +0800

    implement functions

--- main/main.js
+++ main/main.js
@@ -1,9 +1,25 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(a, b) {
+	return a % b;
+};
 
-function caclSum() {};
+function caclSum(input_array) {
+	var sum = 0;
+	for(var i = 0; i < input_array.length; i++){
+		sum += input_array[i];
+	}
+	return sum;
+};
 
-function caclSumInConditon() {};
+function caclSumInConditon(input_array, max) {
+	var sum = 0;
+	for(var i = 0; i < input_array.length; i++){
+		if(input_array[i] < max){
+			sum += input_array[i];
+		}
+	}
+	return sum;
+};
 
 module.exports = {
     caclRemaindar,
